3777a20e77e456d730abd08af95fb488f1bd5ff8
iliaskarim <ilias.karim@gmail.com>
2010-12-08 02:25:32 -0800

    v0.11 Ready to present

---
+++ rgbaColors.h
@@ -0,0 +1,10 @@
+#ifndef __RGBACOLORS__
+#define __RGBACOLORS__
+
+const float g_grey_rgba[] = { .3, .3, .3, 1 };
+const float g_red_rgba[] = { .5, 0, 0, 1 };
+const float g_blue_rgba[] = { 65.0f / 255.0f, 105.0f / 255.0f, 225.0f / 255.0f, 1 };
+const float g_white_rgba[] = { .8, .8, .8, 1 };
+const float g_black_rgba[] = { .1, .1, .1, 1 };
+
+#endif
